474a3fdddd02b3e0c250d6746c84331583fb0a86
drpto <dr.juniornoob@gmail.com>
2024-03-10 04:09:57 +0530

    stone: fixed prebuilt kernel

--- blaze_stone.mk
+++ blaze_stone.mk
@@ -29,7 +29,7 @@ BLAZE_MAINTAINER := Nomi noob
 TARGET_USE_PIXEL_CHARGER := true
 
 # Use Prebuilt Kernel
-PREBUILT_KERNEL := true
+#PREBUILT_KERNEL := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := blaze_stone
