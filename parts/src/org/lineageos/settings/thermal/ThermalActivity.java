af8b8afadc821de1761e91b66e9f2e79edf4e14d
drpto <dr.juniornoob@gmail.com>
2024-03-31 04:24:09 +0530

    stone: parts: Fix compile after QPR2 merge
    Signed-off-by: Nomishaw21 <nomishaw9@hmail.com>

--- parts/src/org/lineageos/settings/thermal/ThermalActivity.java
+++ parts/src/org/lineageos/settings/thermal/ThermalActivity.java
@@ -19,7 +19,7 @@ package org.lineageos.settings.thermal;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
+import com.android.settingslib.collapsingtoolbar.R;
 
 public class ThermalActivity extends CollapsingToolbarBaseActivity {
     private static final String TAG_THERMAL = "thermal";
